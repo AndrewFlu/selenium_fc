02d1d0f14b49e1eaf058938452a44a893ce3e269
Andrew <andreyshalagin@gmail.com>
2019-04-19 22:10:31 +0300

    - removed import

--- selenium-java/src/test/java/ru/moneta/webdriver/TestBase.java
+++ selenium-java/src/test/java/ru/moneta/webdriver/TestBase.java
@@ -3,7 +3,6 @@ package ru.moneta.webdriver;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
