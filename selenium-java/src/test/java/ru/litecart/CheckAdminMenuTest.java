ef5babe6e6275447d54a95a779fc4f37c467a593
Andrew <andreyshalagin@gmail.com>
2019-04-20 08:29:29 +0300

    - добавлен тест на проверку меню администратора. Если есть подменю - наличие заголовков в них тоже проверяется - рефакторинг. Выделен базовый класс TestBase. В нем выделены общие методы

---
+++ selenium-java/src/test/java/ru/litecart/CheckAdminMenuTest.java
@@ -0,0 +1,42 @@
+package ru.litecart;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+public class CheckAdminMenuTest extends TestBase {
+
+    @Test
+    public void testAdminMenu() {
+        String username = "admin";
+        String password = "admin";
+        String baseUrl = "http://localhost/litecart/admin/login.php";
+
+        driver.get(baseUrl);
+        login(username, password);
+
+        List<WebElement> menuItems = driver.findElements(By.cssSelector("ul.list-vertical > li"));
+
+
+        for (int i = 1; i <= menuItems.size(); i++) {
+
+
+            WebElement menuItem = driver.findElement(By.cssSelector(String.format("ul.list-vertical > li:nth-of-type(%d)", i)));
+            menuItem.click();
+            assertTrue(isElementPresent(By.tagName("h1")));
+
+            List<WebElement> childItems = driver.findElements(By.cssSelector("ul.docs li"));
+            for (int k = 1; k <= childItems.size(); k++) {
+
+                WebElement childItem = driver.findElement(By.cssSelector(String.format("ul.docs li:nth-of-type(%d)", k)));
+                childItem.click();
+                assertTrue(isElementPresent(By.cssSelector("h1")));
+            }
+        }
+    }
+
+}
---
+++ selenium-java/src/test/java/ru/litecart/TestBase.java
@@ -0,0 +1,35 @@
+package ru.litecart;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    WebDriver driver;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+    }
+
+    protected void login(String username, String password) {
+        driver.findElement(By.cssSelector("input[name=username]")).sendKeys(username);
+        driver.findElement(By.cssSelector("input[name=password]")).sendKeys(password);
+        driver.findElement(By.cssSelector("button[name=login]")).click();
+    }
+
+    protected boolean isElementPresent(By locator) {
+        return driver.findElements(locator).size() > 0;
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
